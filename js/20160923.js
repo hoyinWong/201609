093c794f89121fcd8ca277ada5db7c39b180c3b6
wanghaiying <wanghaiying@yy.com>
2016-09-23 15:17:45 +0800

    数组去重

--- js/20160923.js
+++ js/20160923.js
@@ -6,9 +6,10 @@
         len = arr.length,
         i, iItem, j, jItem,
         currentTxt;
-        $('.original').text(originalTxt);
-
-    for(var i = 0; i < len; i++){
+        $('.json .original').text(originalTxt);
+        $('.hoyin .original').text(originalTxt);
+    //Json的方法
+    for(i = 0; i < len; i++){
         iItem = arr[i];
         for(j = i+1; j < len; j++){
             jItem = arr[j];
@@ -16,7 +17,23 @@
                 arr.splice(j,1);
             }
         }
-        currentTxt = arr.join('!');
-        $('.current').text(currentTxt);
     }
+    currentTxt = arr.join('!');
+    $('.json .current').text(currentTxt);
+    //hoyin 的方法
+    var 
+        result = [],//用来存放去重后的数组
+        k, kItem, z, zItem,
+        hoyinTxt;
+    for(k = 0; k <len; k++){
+        kItem = arr[k];
+        for(z = k+1; z < len; z++){
+            zItem = arr[z];
+            if(kItem !== zItem){
+                result.push(kItem);
+            }
+        }
+    }
+    hoyinTxt = result.join('!');
+    $('.hoyin .current').text(currentTxt);
 })(jQuery)
